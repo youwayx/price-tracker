5d15c81ed99471df06c057b527da70ab68f72bce
Ioana Crant <ioana.crant@gmail.com>
2015-07-18 15:14:00 -0700

    amazon crawler

---
+++ amazon/spiders/amazon_cool_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+from amazon.items import AmazonItem
+
+class AmazonSpider(scrapy.Spider):
+    name = "amazon_cool"
+    allowed_domains = ["amazon.com"]
+    start_urls= ["http://www.amazon.com/Fire-TV-streaming-media-player/dp/B00CX5P8FC"]
+    #start_urls=["http://www.amazon.com/Amazon-W87CUN-Fire-TV-Stick/dp/B00GDQ0RMG"]
+    def parse(self, response):
+        for price in response.xpath('//*[@id="priceblock_ourprice"]'):
+            print(price.xpath('text()').extract())
+            item=AmazonItem()
+            item['price']=price.xpath('text()').extract()
+            yield item
